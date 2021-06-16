46ccda12feea96b271bc8675a4c5d42ee41d6607
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-06-16 15:02:04 -0500

    created variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,24 @@
+initPage();  function pageInit () {
 
+    const inputEl = document.getElementById("city-search");
+    const searchBtnEl = document.getElementById("search-button");
+    const historyEl = document.getElementById("history")
+    const clearHistoryEl = document.getElementById("clear-history-btn");
+    const cityNameEL = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-picture");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");
+    const currentUvEl = document.getElementById("UV-index");
+
+    let searchHistory = Json.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+
+
+} 
+
+
+
+    
 
 
 
