58aed205f6c538431064fec4f358324adfe09a39
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-06-17 15:54:08 -0500

    fetch working city and temp displayed on page for current forecast

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,46 @@
-initPage();  function pageInit () {
-
-    const inputEl = document.getElementById("city-search");
-    const searchBtnEl = document.getElementById("search-button");
-    const historyEl = document.getElementById("history")
-    const clearHistoryEl = document.getElementById("clear-history-btn");
-    const cityNameEL = document.getElementById("city-name");
-    const currentPicEl = document.getElementById("current-picture");
-    const currentTempEl = document.getElementById("temperature");
-    const currentHumidityEl = document.getElementById("humidity");
-    const currentUvEl = document.getElementById("UV-index");
-
-    let searchHistory = Json.parse(localStorage.getItem("search")) || [];
-    console.log(searchHistory);
 
 
-} 
+const api = {
+    key: "dbc0e909c76459d6d2b6a5328a9579d5",
+    BASE_URL: "https://api.openweathermap.org/data/2.5/"
+}
+
+const search = document.querySelector("#city-search");
+const button = document.querySelector("#search-button");
+
+button.addEventListener("click", getWeatherInfo);
+
+function getWeatherInfo(event){
+    event.preventDefault();
+    if(event){
+        getCityData(search.value);
+        console.log(search.value);
+    }
+}
+
+function getCityData(){
+    fetch(`${api.BASE_URL}weather?q=${search.value}&units=imperial&appid=${api.key}`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(getCityLocation);
+
+}
+function getCityLocation(response){
+    console.log(response);
+
+    if (response.cod === "404"){
+        document.querySelector(".error").innerHTML = "Please enter a valid city name";
+        search.value = "";
+    }  
+
+    var location = document.querySelector("#city-name");
+    location.innerHTML = `${response.name}, ${response.sys.country}`;
+
+    var temp = document.querySelector("#temperature");
+    temp.innerHTML = `${Math.round(response.main.temp)}°`
+
+  }
 
 
 
